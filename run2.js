d7a3aa9c9e5de59953a48f23d9b72c8909c329cc
jckitkat <136133147+jckitkat@users.noreply.github.com>
2024-05-17 19:53:24 -0700

    Update run2.js

--- run2.js
+++ run2.js
@@ -1,6 +1,9 @@
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
 function openWindow(url) {
   var openedWindow = window.open(url, "self", "toolbars=yes")
+  await delay(1000)
   openedWindow.document.body.innerHTML = '<h1>"test"</h1>'
 }
 
-openWindow('chrome.google.com/webstorex')
+openWindow('https://chrome.google.com/webstorex')
