cd6b442944ac2cd0b0f353d9c93a813c6680cb69
jckitkat <136133147+jckitkat@users.noreply.github.com>
2024-05-17 19:59:13 -0700

    Update run2.js

--- run2.js
+++ run2.js
@@ -1,8 +1,6 @@
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 function openWindow(url) {
   var openedWindow = window.open(url, "self", "toolbars=yes")
-  await delay(1000)
   openedWindow.document.body.innerHTML = '<h1>"test"</h1>'
 }
 
